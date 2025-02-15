9f2074915317e242a8362702772e3a5b1dc3b36d
fuad <fuadshorlarxwix@gmail.com>
2025-02-15 18:43:19 +0100

    installed clsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import { useState } from "react";
 import { languages } from "./languages";
 
 export default function AssemblyEndgame() {
+  const [currentWord, setCurrentWord] = useState("react");
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  console.log(guessedLetters);
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  function addGuessedLetter(letter) {
+    setGuessedLetters((prevLetters) =>
+      prevLetters.includes(letter) ? prevletters : [...prevLetters, letter]
+    );
+  }
+
   const languageElements = languages.map((lang) => {
     const styles = {
       backgroundColor: lang.backgroundColor,
       color: lang.color,
     };
-    return <span style={styles}>{lang.name}</span>;
+
+    return (
+      <span className="chip" style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    );
   });
+
+  const letterElements = currentWord
+    .split("")
+    .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
+
+  const keyboardElements = alphabet.split("").map((letter, index) => (
+    <button key={index} onClick={() => addGuessedLetter(letter)}>
+      {letter.toUpperCase()}
+    </button>
+  ));
+
   return (
     <main>
       <header>
@@ -23,6 +51,9 @@ export default function AssemblyEndgame() {
         <p>Well done! 🎉</p>
       </section>
       <section className="language-chips">{languageElements}</section>
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="new-game">New Game</button>
     </main>
   );
 }
