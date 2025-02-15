2d18354f87d40cc0c59f65b0b4400783a7633cbb
fuad <fuadshorlarxwix@gmail.com>
2025-02-15 03:45:31 +0100

    you win section

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function AssemblyEndgame() {
+  return (
+    <main>
+      <header>
+        <h1>Asssembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+    </main>
+  );
+}
